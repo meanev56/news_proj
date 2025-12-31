e370ec210467146590b5e8018a42fa7c90a1dc88
meanev56 <emekanjoku86@gmail.com>
2025-12-31 14:52:23 +0100

    updated logoheader and added logo

--- components/LogoHeader.tsx
+++ components/LogoHeader.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import  Image  from 'next/image';
 
-const LogoHeader = () => {
+export default function LogoHeader() {
   return (
-    <div>LogoHeader</div>
+    <header className="bg-white text-black shadow-md">
+      <div className='container mx-auto p-4 flex justify-center items-center '>
+        <div className='justify-center'>
+          <div className='text-center'>
+            <Image 
+              src="/images/businessday.png"
+              alt="BusinessDay Logo"
+              className='mx-auto sm:w-20 md:w-26 lg:w-64 transition-all'
+              width={50}
+              height={24}
+            />
+          </div>
+          <div className="flex justify-between text-black mt-4 font-bold text-xs md:text-sm">
+              <h1 className='mx-3'>TRACKING TRENDS</h1>
+              <h1 className='mx-4'>  | </h1>
+              <h1>INFORMING DECISIONS</h1>
+            </div>
+        </div>
+      </div>
+    </header>
   )
 }
-
-export default LogoHeader
